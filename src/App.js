275d79080b7b053f2117169934a7aaddbe9e2d1d
Shaurya <shaunkapoor170@gmail.com>
2019-09-26 13:34:11 -0700

    API calls + tricking the server to think it is a real server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./components/Form";
+import { async } from 'q';
+
+//Always a good idea to store your imported data into a constant 
+const API_KEY = "499f59092da31cfd528fe53ff3119a93"
+
+//https://www.food2fork.com/api/search?key=&q=shredded%20chicken&count=5
+
 
 class App extends Component {
   //This method will be responsible for making the API call 
   //In the new React you can even declare a state without a constuctor function
   //e is the event object in JS not React 
-  getRecipe = (e) => {
+  //Will make use of Acincobait and fetch API to make a call to call
+  getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log(recipeName);
+    const api_call = await fetch
+    (`http://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
+
+    const data = await api_call.json();
+    console.log(data);
   }
   render() {
     return (
