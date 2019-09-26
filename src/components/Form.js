eb6b209be835a208a5c09430b7bd8026893a0be4
Shaurya <shaunkapoor170@gmail.com>
2019-09-26 11:40:07 -0700

    Added Components and Form.js(3:57)

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//creating a stateless functional componenet instead of a class base component 
+const Form = () => (
+    <form>
+        <input type="text"/>
+        <button>Search</button>
+    </form>
+);
+
+export default Form;
