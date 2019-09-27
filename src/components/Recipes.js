f0523ccd91264bf21075bae235f7ca0f80d6b658
Shaurya <shaunkapoor170@gmail.com>
2019-09-27 13:35:40 -0700

    Making code cleaner

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 const Recipes = (props) => (
-    <div>
-        { props.recipes.map((recipe) => {
-          return (
-            <div key={recipe.recipe_id}>
-              <img src={recipe.image_url} alt={recipe.title}></img>
-              <p>{recipe.title }</p>
-
-            </div>
-        );
+  <div className="container">
+    <div className="row">
+    { props.recipes.map((recipe) => {
+      return (
+        <div className="col-md-4">
+          <div className="recipe__box">
+            <img
+              className="recipe__box-img"
+              src={recipe.image_url}
+              alt={recipe.title}/>
+              <div className="recipe__text">
+                <h5>{ recipe.title }</h5>
+              </div>
+          </div>
+        </div>
+      );
     })}
     </div>
+  </div>
 );
 
 export default Recipes;
