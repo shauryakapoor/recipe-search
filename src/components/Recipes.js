0fe7a0d0394d6726ee5dc8acfb00de2183a4df7a
Shaurya <shaunkapoor170@gmail.com>
2019-09-28 19:04:50 -0700

    Added new component called Router.js

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,7 +5,8 @@ const Recipes = (props) => (
     <div className="row">
     { props.recipes.map((recipe) => {
       return (
-        <div key = {recipe.title} className="col-md-4" style = {{ marginBottom: "2rem"}}>
+        <div key = {recipe.title} className="col-md-4" 
+        style = {{ marginBottom: "2rem"}}>
           <div className="recipes__box">
             <img
               className="recipe__box-img"
@@ -13,7 +14,8 @@ const Recipes = (props) => (
               alt={recipe.title}/>
               <div className="recipe__text">
                 <h5 className="recipes__title" >
-                  { recipe.title.length < 20 ? `${recipe.title}` :`${recipe.title.substring(0, 25)}...` }
+                  { recipe.title.length < 20 ? `${recipe.title}` :
+                  `${recipe.title.substring(0, 25)}...` }
                 </h5>
                 <p className="recipes__subtitle">Publisher: <span>
                     { recipe.publisher }
