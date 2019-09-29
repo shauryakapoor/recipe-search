7a7220ac0b94bdaba067df90a88a08bfa4d1a196
Shaurya <shaunkapoor170@gmail.com>
2019-09-28 18:36:04 -0700

    Fixing the styling of the button- View Recipe

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,18 +6,18 @@ const Recipes = (props) => (
     { props.recipes.map((recipe) => {
       return (
         <div key = {recipe.title} className="col-md-4" style = {{ marginBottom: "2rem"}}>
-          <div className="recipe__box">
+          <div className="recipes__box">
             <img
               className="recipe__box-img"
               src={recipe.image_url}
               alt={recipe.title}/>
               <div className="recipe__text">
-                <h5 className="recipe__title" >{ recipe.title }</h5>
-                <p className="recipe__subtitle">Publisher: <span>
+                <h5 className="recipes__title" >{ recipe.title }</h5>
+                <p className="recipes__subtitle">Publisher: <span>
                     { recipe.publisher }
                   </span></p>
               </div>
-              <button className="recipe__button" >View Recipe</button>
+              <button className="recipe_buttons" >View Recipe</button>
           </div>
         </div>
       );
