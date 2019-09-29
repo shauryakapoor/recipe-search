89b65ee4451576827f0efbba9442d668964c8379
Shaurya <shaunkapoor170@gmail.com>
2019-09-28 19:24:37 -0700

    Rendering out recipe component with the help of keyword exact

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,7 +7,7 @@ import Recipe from "./Recipe";
 const Router = () => (    
     <BrowserRouter>
         <Switch>
-            <Route path="/" component={App} />
+            <Route path="/" component={App} exact />
             <Route path="/recipe" component={Recipe} />
         </Switch>
     </BrowserRouter>
