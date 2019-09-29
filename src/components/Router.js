691cb537117346dba7d91cded383303c12bf45ed
Shaurya <shaunkapoor170@gmail.com>
2019-09-28 19:08:20 -0700

    Added another file/component - Recipe.js. Note: This is singular

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "../App";
 
-const Router = () => {
-<BrowserRouter>
-    <Route path="/" component={App} />
-</BrowserRouter>
+const Router = () => {    
+    <BrowserRouter>
+        <Route path="/" component={App} />
+    </BrowserRouter>
 };
